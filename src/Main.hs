49c1b44bb6049aaca5eae2cdca5d3a69e94bab8e
Aditya Bhargava <bluemangroupie@gmail.com>
2013-03-29 15:33:40 -0500

    whups

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,11 @@
 import Web.Scotty
 import System.IO.Unsafe
 import qualified Data.Text.Lazy as T
+import qualified Data.Text.Encoding as E
 import qualified Data.ByteString as BS
 import System.Environment
 
-contents = (T.decodeUtf8With lenientDecode) . unsafePerformIO $ BS.readFile "src/blogpost.html"
+contents = (E.decodeUtf8With E.lenientDecode) . unsafePerformIO $ BS.readFile "src/blogpost.html"
 
 main = do
   port <- getEnv "PORT"
