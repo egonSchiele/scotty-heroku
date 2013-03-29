7ad8a6dd6038aaa459472e844abcf7bf17325640
Aditya Bhargava <bluemangroupie@gmail.com>
2013-03-29 15:03:29 -0500

    sigh same mistake

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,13 @@
 import Web.Scotty
 import System.IO.Unsafe
 import qualified Data.Text.Lazy as T
+import System.Environment
 
 contents = T.pack . unsafePerformIO $ readFile "blogpost.html"
 
-main = scotty 8234 $ do
-  get "/" $ do
-    html contents
+main = do
+  port <- getEnv "PORT"
+  scotty (fromIntegral $ read port) $ do
+    get "/" $ do
+      html contents
 
