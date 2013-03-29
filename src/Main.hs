77dd3ea7432eb9c7ceffa7125caeb20646acf1d6
Aditya Bhargava <bluemangroupie@gmail.com>
2013-03-29 15:29:01 -0500

    lets try this to work around utf-8 issue

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,10 @@
 import Web.Scotty
 import System.IO.Unsafe
 import qualified Data.Text.Lazy as T
+import qualified Data.ByteString as BS
 import System.Environment
 
-contents = T.pack . unsafePerformIO $ readFile "src/blogpost.html"
+contents = (T.decodeUtf8With lenientDecode) . unsafePerformIO $ BS.readFile "src/blogpost.html"
 
 main = do
   port <- getEnv "PORT"
