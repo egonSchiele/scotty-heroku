2434143fbed5b963b2b3af0c1fdb11788ace6b66
Aditya Bhargava <bluemangroupie@gmail.com>
2013-03-29 16:01:02 -0500

    testing new buildpak

--- src/Main.hs
+++ src/Main.hs
@@ -14,4 +14,7 @@ main = do
   scotty (fromIntegral $ read port) $ do
     get "/" $ do
       html contents
-
+  
+    get "/:word" $ do
+      beam <- param "word"      
+      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
